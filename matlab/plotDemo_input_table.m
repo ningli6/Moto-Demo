f87220c88624132a5deafda6502750c2fb9c2c93
ningli <ningli@vt.edu>
2015-03-24 18:27:10 -0400

    Fix plotting in Matlab
    Can plot contour for only 1 set of values
    
    specify color range for color bar

--- matlab/plotDemo_input_table.m
+++ matlab/plotDemo_input_table.m
@@ -7,13 +7,13 @@ latStart = 45;
 latEnd = 50;
 LongStart = -100;
 LongEnd = -95;
-PU_Lat = 49;
-PU_Lon = -99;
+PU_Lat = 46;
+PU_Lon = -96;
 cellSize = 0.1;
 rows = 5 / cellSize;
 cols = 5 / cellSize;
 
-importNumber = '0';
+importNumber = '3';
 % import data from a text file
 importName = ['/Users/ningli/Desktop/demoTable_', importNumber, '.txt'];
 import = importdata(importName);
@@ -27,43 +27,65 @@ end
 
 x = (LongStart):((LongEnd - LongStart)/(cols - 1)):(LongEnd);
 y = (latStart):((latEnd - latStart)/(rows - 1)):(latEnd);
-% [X,Y] = meshgrid(x,y);
-[m,n] = size(M);
+x2 = (LongStart):((LongEnd - LongStart)/(2 * cols - 1)):(LongEnd);
+y2 = (latStart):((latEnd - latStart)/(2 * rows - 1)):(latEnd);
+[X,Y] = meshgrid(x, y);
+[X2,Y2] = meshgrid(x2, y2);
+vq = interp2(X,Y,M,X2,Y2);
+[m,n] = size(vq);
 
 inverseM = zeros(m, n);
 for i = 1: m
-    inverseM(i, :) = M(m - i + 1, :);
+    inverseM(i, :) = vq(m - i + 1, :);
 end
 
-minP = 0.5;
-maxP = max(max(inverseM));
-plotrange = minP:(maxP - minP)/4:maxP;
+% minP = 0.5;
+% maxP = max(max(inverseM));
+% plotrange = minP:(maxP - minP)/4:maxP;
 
 copy = inverseM;
 for i = 1: m
     for j = 1 : n
-        if copy(i, j) > plotrange(1, 1) && copy(i, j) < plotrange(1, 2)
-            copy(i, j) = 0.5;
-        else if copy(i, j) >= plotrange(1, 2) && copy(i, j) < plotrange(1, 3)
-                copy(i, j) = 0.6;
-            else if copy(i, j) >= plotrange(1, 3) && copy(i, j) < plotrange(1, 4)
-                    copy(i, j) = 0.7;
-                else if copy(i, j) >= plotrange(1, 4) && copy(i, j) < plotrange(1, 5)
-                        copy(i, j) = 0.8;
-                    else if copy(i, j) >= plotrange(1, 5)
-                            copy(i, j) = 0.9;
-                        else if copy(i, j) == 0
-                                copy(i, j) = 0;
-                            else
-                                copy(i, j) = NaN;
-                            end
-                        end
-                    end
-                end
-            end
+        if copy(i, j) == 0
+            copy(i, j) = NaN;
+%         else if copy(i, j) > 0.5
+%                 copy(i, j) = 0.7;
+%             end
         end
     end
 end
+% for i = 1: m
+%     for j = 1 : n
+%         if copy(i, j) > plotrange(1, 1) && copy(i, j) < plotrange(1, 2)
+%             copy(i, j) = 0.5;
+%         else if copy(i, j) >= plotrange(1, 2) && copy(i, j) < plotrange(1, 3)
+%                 copy(i, j) = 0.6;
+%             else if copy(i, j) >= plotrange(1, 3) && copy(i, j) < plotrange(1, 4)
+%                     copy(i, j) = 0.7;
+%                 else if copy(i, j) >= plotrange(1, 4) && copy(i, j) < plotrange(1, 5)
+%                         copy(i, j) = 0.8;
+%                     else if copy(i, j) >= plotrange(1, 5)
+%                             copy(i, j) = 0.9;
+%                         else if copy(i, j) == 0
+%                                 copy(i, j) = 0;
+%                             else
+%                                 copy(i, j) = NaN;
+%                             end
+%                         end
+%                     end
+%                 end
+%             end
+%         end
+%     end
+% end
+
+% for i = 1: m
+%     for j = 1 : n
+%         if copy(i, j) == 0
+%             copy(i, j) = NaN;
+%         end  
+%     end
+% end
 
 % % change entries value to gain better output map
 % I = zeros(length(A), 1);
@@ -81,22 +103,27 @@ end
 
 % plot on google map
 figure();
-contourf(x,y,copy);
+% plot_google_map('maptype','hybrid','APIKey','AIzaSyDMSjokr-3WVHSYtZeW5xM2gI6uO8BkiMI');
+contourf(x2, y2, copy);
 title('Input interpolation by griddata');
 xlabel('longitude');
 ylabel('latitude');
 % plot color bar
-contourcmap('jet', 'Colorbar', 'on', ...
-   'Location', 'vertical', ...
-   'ColorAlignment', 'center',...
-   'TitleString', 'Probability value');
+caxis([0.4, 0.6]);
+% caxis auto;
+colorbar;
+% contourcmap('jet', 'Colorbar', 'on', ...
+%    'Location', 'vertical', ...
+%    'ColorAlignment', 'center',...
+%    'TitleString', 'Probability value');
 hold on;
 % draw the PU location on the map
-plot(PU_Lon ,PU_Lat,'ko','MarkerSize',20);
+plot(PU_Lon ,PU_Lat,'r*','MarkerSize',20);
 % drwa google map
 plot_google_map('maptype','hybrid','APIKey','AIzaSyDMSjokr-3WVHSYtZeW5xM2gI6uO8BkiMI');
+% colormap default;
 hold off;
-
+% 
 % Output the contours into pdf and png file
 fileextension = '.pdf';
 name = ['/Users/ningli/Desktop/', userid, '_', filename, '_probability_', importNumber, fileextension];
