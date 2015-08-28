26d061d890479ce4fbd29f7de644f3153cd65ccc
ningli <ningli@vt.edu>
2015-08-28 11:44:52 -0400

    Add explaination for drawing anaylsis region
    Modify code for channel button with JQuery hide/show functions

--- web/channels.js
+++ web/channels.js
@@ -22,34 +22,19 @@ function setChannels(args) {
     numberOfChannels = parseInt(args);
     switch (numberOfChannels) {
     case 1:
-        console.log("Number of channels: 1");
-        chanls = 1
-        var str = "<button type='button' class='btn btn-warning' onclick='resetAllMarkers();'>Reset</button>";
-        str += '<span class="help-block">Click rectangle icon to draw analysis area</span>'
-        str += "<div id='map-canvas' style='width:100%; height:500px;'></div>";
-        document.getElementById("mapArea").innerHTML = str;
-        window.onload = initialize();
+        $("#butnChannel1").show();
+        $("#butnChannel2").hide();
+        $("#butnChannel3").hide();
         break;
     case 2:
-        console.log("Number of channels: 2");
-        var str = "<button type='button' id = 'tryBut' class='btn btn-info' onclick='chanls = 0;'>Select location of PU(s) for channel 0</button>";
-        str += " <button type='button' class='btn btn-info' onclick='chanls = 1;'>Select location of PU(s) for channel 1</button>";
-        str += " <button type='button' class='btn btn-warning' onclick='resetAllMarkers();'>Reset</button>";
-        str += '<span class="help-block">Click rectangle icon to draw analysis area</span>'
-        str += "<div id='map-canvas' style='width:100%; height:500px;'></div>";
-        document.getElementById("mapArea").innerHTML = str;
-        window.onload = initialize();
+        $("#butnChannel1").hide();
+        $("#butnChannel2").show();
+        $("#butnChannel3").hide();
         break;
     case 3:
-        console.log("Number of channels: 3");
-        var str = "<button type='button' class='btn btn-info' onclick='chanls = 0;'>Select location of PU(s) for channel 0</button>";
-        str += " <button type='button' class='btn btn-info' onclick='chanls = 1;'>Select location of PU(s) for channel 1</button>";
-        str += " <button type='button' class='btn btn-info' onclick='chanls = 2;'>Select location of PU(s) for channel 2</button>";
-        str += " <button type='button' class='btn btn-warning' onclick='resetAllMarkers();'>Reset</button>";
-        str += '<span class="help-block">Click rectangle icon to draw analysis area</span>'
-        str += "<div id='map-canvas' style='width:100%; height:500px;'></div>";
-        document.getElementById("mapArea").innerHTML = str;
-        window.onload = initialize();
+        $("#butnChannel1").hide();
+        $("#butnChannel2").hide();
+        $("#butnChannel3").show();
         break;
     }
 }
